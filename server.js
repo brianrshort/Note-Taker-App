5289c58570eee3964edb92680bb8c90fbdb0a27e
Brian Short <brian.ralph.short@gmail.com>
2020-11-21 08:06:18 -0500

    Add port listner in server.js

--- server.js
+++ server.js
@@ -13,3 +13,7 @@ app.use(bodyParser.json())
 
 require('./routing/api-routes.js')(app);
 require('./routing/html-routes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
