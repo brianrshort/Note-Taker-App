bf787a5d6d77e8884e7f2dd1d5c508d5f5a7b963
Brian Short <brian.ralph.short@gmail.com>
2020-11-21 08:01:29 -0500

    Add data folder, notes object array

---
+++ data/notes.js
@@ -0,0 +1,10 @@
+var notesArray = [
+    {
+        noteDate: "Tuesday, November 17, 2020",
+        noteTopic: "Dogs",
+        noteNote: "I love dogs!",
+        noteID: "K9"
+    }
+]
+
+module.exports = notesArray;
